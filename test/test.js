b10c7da94460ed4d638f3cd44beecf7d14357c38
benjamingitt <89865918+benjamingitt@users.noreply.github.com>
2023-11-21 12:13:14 +0300

    Update test.js

--- test/test.js
+++ test/test.js
@@ -1,5 +1,5 @@
 const User = require('../models/User');
-const app = require('../server'); // Импортируем ваш сервер
+const app = require('../server'); 
 const request = require('supertest');
 const expect = require('chai').expect;
 
